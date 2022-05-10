00c5560f6ac6c8908d8168b58b9cd3aa0c38c911
Aydin Kuru <aydinkuru81@gmail.com>
2022-05-10 23:52:49 +0200

    s6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="app">React App</div>;
+  return (
+    // BEM
+    <div className="app">
+      <h1> Hello Clever Programmers, let's build the Amazon</h1>
+      {/*Header */}
+      {/* Home */}
+    </div>
+  );
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from "react";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
+      // eslint-disable-next-line jsx-a11y/alt-text
       <img
         className="header__logo"
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
       />
-
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         {/* Logo */}
       </div>
-
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
